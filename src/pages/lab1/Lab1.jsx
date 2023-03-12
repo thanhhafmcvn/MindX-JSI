b92c96ef694023502a1aff8bfe047db0e86a169e
mlucastero <thanhhafmcvn@outlook.com.vn>
2023-03-12 12:29:43 +0700

    working COVID19 website

--- src/pages/lab1/Lab1.jsx
+++ src/pages/lab1/Lab1.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import Main from './components/Main'
+import React, { useState, useEffect } from "react";
+import Information from "./components/Information";
+import axios from "axios";
 const Lab1 = () => {
+  const [apiResponse, setApiResponse] = useState({});
+  const APIEndpoint = "https://api.covid19api.com/summary";
+  const handleGetData = async () => {
+    const response = await axios.get(APIEndpoint);
+    const data = response.data;
+    setApiResponse(data);
+  };
+  useEffect(() => {
+    handleGetData();
+  }, []);
+  console.log(apiResponse)
   return (
-      <div>
-          <h1 className='text-black font-semibold text-[30px] text-center'>
-              COVID-19 Statistics website using ReactJS
-          </h1>
-          <Main />
+    <div>
+      <h1 className="text-center text-[64px] font-semibold">
+        COVID19 WEBSITE USING REACTJS
+      </h1>
+      <Information
+        cases={apiResponse?.Global?.TotalConfirmed}
+        deaths={apiResponse?.Global?.TotalDeaths}
+        recovered={apiResponse?.Global?.TotalRecovered}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Lab1
+export default Lab1;
--- src/pages/lab1/components/Information.jsx
+++ src/pages/lab1/components/Information.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Main from "./Main";
+
 const Information = (props) => {
-  const { cases, deaths, recovered} = props;
+  const { cases, deaths, recovered } = props;
   return (
     <div>
       <h1>Total cases: {cases}</h1>
-      <h1>Total deaths: {deaths} </h1>
+      <h1>Total deaths: {deaths}</h1>
       <h1>Total recovered: {recovered}</h1>
     </div>
   );
